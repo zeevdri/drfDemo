31e7120dbf67eed44629c0770a2165685cc607a2
zeev <zeev@percepto.co>
2021-01-05 12:00:52 +0200

    - create schema - added demo documentation

--- people/views/people.py
+++ people/views/people.py
@@ -1,6 +1,13 @@
 """
 1. Implement PersonSerializer
 2. Implement PersonViewSet
+   - talk about actions (readonly/write), list_route, detail_route
+     - list
+     - retreive
+     - create
+     - delete
+     - update
+   - people/fullname endpoint
 3. add to urls
 4. add PersonSerializer.city_name
 5  add CitySerializer nested
@@ -8,6 +15,11 @@
 7. add filtering
    - by first_name, last_name
 8. auto client code creation
+   - openapi/ endpoint
+   - schema generation
+     - run ./manage.py generateschema --file ./client/schema/openapi-schema.yml
+   - client code generation
+     -
 """
 from rest_framework import viewsets, pagination
 
