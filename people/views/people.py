579b01eae356060ca1ed707eb40087465bfea501
zeev <zeev@percepto.co>
2021-01-05 12:59:12 +0200

    - added demo documentation - added required tsconfig for ts-node demonstration

--- people/views/people.py
+++ people/views/people.py
@@ -1,6 +1,13 @@
 """
 1. Implement PersonSerializer
 2. Implement PersonViewSet
+   - talk about actions (readonly/write), list_route, detail_route
+     - list
+     - retreive
+     - create
+     - delete
+     - update
+   - people/fullname endpoint
 3. add to urls
 4. add PersonSerializer.city_name
 5  add CitySerializer nested
@@ -8,4 +15,13 @@
 7. add filtering
    - by first_name, last_name
 8. auto client code creation
+   - openapi/ endpoint
+   - schema generation
+     - run ./manage.py generateschema --file ./client/schema/openapi-schema.yml
+   - client code generation
+     - nvm use v14; node ./node_modules/.bin/openapi-generator-cli generate -i ./client/schema/openapi-schema.yml -o ./client/src -g typescript-axios
+     - ts-node -i
+       > import { ApiApiFactory } from "./client/src";
+       > let api = ApiApiFactory(undefined, "http://127.0.0.1:8000");
+       > api.listPersons().then(r => console.info(r.data));
 """
